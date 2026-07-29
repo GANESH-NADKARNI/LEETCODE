bf9cf2d4474d80cb486cdf27c74632e798fe97af
GANESH NADKARNI <ganeshnadkarni369@gmail.com>
2026-07-29 22:07:09 +0530

    Backfill: 1603. Design Parking System (cpp)

---
+++ 1603-design-parking-system/1603-design-parking-system.cpp
@@ -0,0 +1,26 @@
+class ParkingSystem {
+public:
+    int slot[3];
+
+    ParkingSystem(int big, int medium, int small) {
+        slot[0] = big;
+        slot[1] = medium;
+        slot[2] = small;
+
+    }
+    
+    bool addCar(int carType) {
+
+        if(slot[carType-1] > 0){
+            slot[carType-1]--;
+            return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
