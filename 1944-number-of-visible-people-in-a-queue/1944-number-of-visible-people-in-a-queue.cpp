10cc009150cc7ba7c44ce90e395d775bb0cc03c6
GANESH NADKARNI <ganeshnadkarni369@gmail.com>
2026-07-29 22:34:59 +0530

    Backfill: 1944. Number of Visible People in a Queue (cpp)

---
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> canSeePersonsCount(vector<int>& heights) {
+        int n=heights.size();
+
+        stack<int>st;
+        vector<int>vis(n,-1);
+
+        for(int i=n-1;i>=0;i--){
+            int count=0;
+            while(!st.empty() && heights[st.top()]<heights[i]){
+                st.pop();
+                count++;
+            }
+            if(!st.empty()) count++;
+            vis[i]=count;
+            st.push(i);
+        }
+        return vis;
+    }
+    
+
+};
