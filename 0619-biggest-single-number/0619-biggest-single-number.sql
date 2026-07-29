17f1f11e461693be8f55bc2de2235e00c9bd4279
GANESH NADKARNI <ganeshnadkarni369@gmail.com>
2026-07-29 22:16:44 +0530

    Backfill: 619. Biggest Single Number (mysql)

---
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+
+SELECT MAX(num) as num
+FROM mynumbers 
+where num IN (
+    SELECT num
+    FROM mynumbers
+    GROUP BY num
+    HAVING COUNT(*)=1
+)
