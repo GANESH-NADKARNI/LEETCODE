814367e3dae3cc6950f51eb7958cb41351952d14
GANESH NADKARNI <ganeshnadkarni369@gmail.com>
2026-07-29 21:48:15 +0530

    Backfill: 1108. Defanging an IP Address (cpp)

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string s;
+        for(int i=0; i<address.size(); i++){
+            if(isdigit(address[i])) s.push_back(address[i]);
+            else {
+                s.push_back('[');
+                s.push_back('.');
+                s.push_back(']');
+            }
+        }
+        return s;
+    }
+};
