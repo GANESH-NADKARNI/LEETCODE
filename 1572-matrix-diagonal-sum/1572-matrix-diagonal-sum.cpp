784be346a9bad32844127dfa4c09ec21ad4f6f0e
GANESH NADKARNI <ganeshnadkarni369@gmail.com>
2026-07-29 22:08:56 +0530

    Backfill: 1572. Matrix Diagonal Sum (cpp)

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int sum = 0,n = mat.size();
+
+        for(int i = 0;i < n;i++){
+            sum += mat[i][i]+mat[i][n-i-1];
+        }
+        if(n%2 != 0){
+            return sum - mat[(n-1)/2][(n-1)/2];
+        }
+        return sum;
+    }
+};
