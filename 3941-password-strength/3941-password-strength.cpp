68c29af8d83fa0203a3c19fb21d78ea9ef8df093
GANESH NADKARNI <ganeshnadkarni369@gmail.com>
2026-08-01 22:44:53 +0530

    Time: 7 ms (87.42%), Space: 17.6 MB (62.43%) - LeetHub

---
+++ 3941-password-strength/3941-password-strength.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int passwordStrength(string s) {
+        int ans=0;
+        unordered_set<char> st(s.begin(),s.end());
+        for (char c : st) {
+            if (c >= 'a' && c <= 'z')
+                ans++;
+            else if (c >= 'A' && c <= 'Z')
+                ans += 2;
+            else if (c >= '0' && c <= '9')
+                ans += 3;
+            else if (c == '!' || c == '@' || c == '#' || c == '$')
+                ans += 5;
+        }
+        return ans;
+    }
+};
