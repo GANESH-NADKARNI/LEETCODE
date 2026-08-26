b10b8d04cd4cc05498aeb4ebf43d601b67de20fa
GANESH NADKARNI <ganeshnadkarni369@gmail.com>
2026-08-26 11:21:01 +0530

    Time: 0 ms (100%), Space: 43.6 MB (79.57%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans;
+
+        for (auto &word : words) {
+            int sum = 0;
+
+            for (char c : word) {
+                sum += weights[c - 'a'];
+            }
+
+            ans += char('z' - (sum % 26));
+        }
+
+        return ans;
+    }
+};
