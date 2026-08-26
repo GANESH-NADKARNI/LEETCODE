14a73fc5997da6aa412699942f479895dd4344c7
GANESH NADKARNI <ganeshnadkarni369@gmail.com>
2026-08-26 11:17:39 +0530

    Time: 0 ms (100%), Space: 7.9 MB (97.54%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int minOperations(int n) {
-        return n * n / 4;
+        int ans = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            ans += n - (2 * i + 1);
+        }
+
+        return ans;
     }
 };
